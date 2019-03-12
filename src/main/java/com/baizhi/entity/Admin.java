c87827e3afcfe5edc6efa69433a325cefe3b324e
zj <3219776072@qq.com>
2019-03-12 16:57:29 +0800

    333333

---
+++ src/main/java/com/baizhi/entity/Admin.java
@@ -0,0 +1,13 @@
+package com.baizhi.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class Admin implements Serializable {
+    private String id;
+    private String username;
+    private String password;
+
+}
